28310b0dd14983c955c1a969da8643046c520ed6
Shubham10soni <shubhsoniyq10@gmail.com>
2025-01-01 19:54:22 +0530

    Semicolonised 2.0| Change in work component

--- components/work.tsx
+++ components/work.tsx
@@ -87,6 +87,83 @@ const Work = () => {
             <h3
               className={`font-normal text-xs dark:text-zinc-400 text-zinc-500 pt-1 ${poppins}`}
             >
+              Associate Consultant
+            </h3>
+            <h3
+              className={`font-normal text-xs dark:text-zinc-400 text-zinc-500 pt-1 ${poppins}`}
+            >
+              (Professional-I)
+            </h3>
+          </div>
+        </div>
+        <div
+          className={`font-light text-xs tracking-wider dark:text-zinc-400 text-zinc-400 pt-6 ${poppins} flex flex-row items-end  justify-end`}
+        >
+          <h4>Jan 2025 - Present</h4>
+        </div>
+        <div className="pt-5 flex flex-row gap-4">
+          <div className="flex  h-10 w-10 rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 justify-center items-center ">
+            <svg
+              viewBox="0 0 1080 1080"
+              xmlns="http://www.w3.org/2000/svg"
+              height={28}
+              width={28}
+            >
+              <g
+                transform="matrix(1, 0, 0, 1, 540, 540)"
+                id="13ad4358-6cda-4534-bc66-b23fa9e40adb"
+              />
+              <g transform="matrix(4.732861042022705, 0, 0, 4.732861042022705, 539.8373413085938, 539.9232788085938)">
+                <g vector-effect="non-scaling-stroke">
+                  <g transform="matrix(7.65 0 0 7.65 38.99 44.28)">
+                    <path
+                      stroke="none"
+                      strokeWidth={1}
+                      strokeDasharray={"none"}
+                      strokeLinecap="butt"
+                      strokeDashoffset={0}
+                      strokeLinejoin="miter"
+                      strokeMiterlimit={4}
+                      fill="rgb(18,171,219)"
+                      fillRule="evenodd"
+                      opacity={1}
+                      vector-effect="non-scaling-stroke"
+                      transform=" translate(-160.26, -19.58)"
+                      d="M 163.434 22.698 C 167.162 22.698 170.041 19.66 170.078 16.058 C 169.81900000000002 14.485 169.268 11.575 165.185 11.575 C 160.721 11.575 159.24 17.814999999999998 155.605 21.837 C 155.31 24.125 153.147 26.172 150.44199999999998 26.515 C 151.10599999999997 27.209 152.57799999999997 27.582 154.33599999999998 27.582 C 157.55599999999998 27.582 161.45399999999998 26.613 163.493 24.6 C 160.773 24.635 159.023 22.887 158.85 20.464000000000002 C 160.177 22.072000000000003 161.677 22.698 163.434 22.698"
+                      stroke-linecap="round"
+                    />
+                  </g>
+                  <g transform="matrix(7.65 0 0 7.65 0 -9.31)">
+                    <path
+                      stroke=" none"
+                      strokeWidth={1}
+                      strokeDasharray={"none"}
+                      strokeLinecap="butt"
+                      strokeDashoffset={0}
+                      strokeLinejoin="miter"
+                      strokeMiterlimit={4}
+                      fill="rgb(0,112,173)"
+                      fillRule="evenodd"
+                      opacity={1}
+                      vector-effect="non-scaling-stroke"
+                      transform=" translate(-155.16, -12.57)"
+                      d="M 170.072 15.793 C 169.991 11.606 167.998 8.073 164.927 5.253 C 162.595 3.123 159.824 1.506 156.919 0.288 C 156.685 0.189 156.45 0.094 156.214 0.001 L 156.213 0.001 C 152.636 4.286 140.25 7.484 140.25 16.468 C 140.25 19.98 142.468 23.264 145.725 24.582 C 147.613 25.296 149.502 25.335 151.392 24.7 C 153.072 24.148 154.454 23.106 155.603 21.836 C 159.241 17.814 160.721 11.576 165.184 11.576 C 169.268 11.576 169.818 14.483 170.079 16.057 C 170.079 16.053 170.077 15.943 170.072 15.793"
+                      stroke-linecap="round"
+                    />
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </div>
+          <div className="flex flex-col items-start justify-center ">
+            <h3
+              className={`font-medium text-sm  text-zinc-700 dark:text-zinc-50  ${poppins}`}
+            >
+              Capgemini
+            </h3>
+            <h3
+              className={`font-normal text-xs dark:text-zinc-400 text-zinc-500 pt-1 ${poppins}`}
+            >
               Senior Analyst
             </h3>
           </div>
@@ -94,7 +171,7 @@ const Work = () => {
         <div
           className={`font-light text-xs tracking-wider dark:text-zinc-400 text-zinc-400 pt-6 ${poppins} flex flex-row items-end  justify-end`}
         >
-          <h4>Oct 2023 - Present</h4>
+          <h4>Oct 2023 - Dec 2024</h4>
         </div>
         <div className="pt-5 flex flex-row gap-4">
           <div className="flex  h-10 w-10 rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 justify-center items-center ">
